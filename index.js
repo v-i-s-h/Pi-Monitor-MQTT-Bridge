a9c5545bf2ff49753f94aadcb96e43fbf8bb5257
v-i-s-h <get.vichu@gmail.com>
2017-08-20 11:00:22 +0530

    Moved configurations to config.json

--- index.js
+++ index.js
@@ -3,33 +3,27 @@
 var pisys   = require( "./pi-system.js" );
 var mqtt    = require( "mqtt" );
 
-var baseTopic   = "user/vish/device/pandora";    // change for each device
+var config  = require( "./config.json" );
 
-var broker  = "mqtt://pandora.local";
-var options = {
-    port: 1883,
-    client_id: 'pi-monitor-bridge'
-};
-
-var conn    = mqtt.connect( broker, options );
+var conn    = mqtt.connect( config.broker.url, config.broker.options );
 
 conn.on( 'connect', function() {
-    console.log( "Connected to broker: " + broker );
+    console.log( "Connected to broker: " + config.broker.url );
     setInterval( function() {
         // Get CPU Temparature
-        msg = { value: pisys.getCpuTemparature(), unit: "'C" }; 
-        conn.publish( baseTopic + "/temp/cpu", JSON.stringify(msg) );
+        conn.publish( config.topic.base + "/" +config.topic.temp_cpu, 
+                        JSON.stringify({value:pisys.getCpuTemparature(),unit:"'C"} ) );
         // Get GPU Temparature
-        msg = { value: pisys.getGpuTemparature(), unit: "'C" };
-        conn.publish( baseTopic + "/temp/gpu", JSON.stringify(msg) );
+        conn.publish( config.topic.base + "/" +config.topic.temp_gpu, 
+                        JSON.stringify({value:pisys.getGpuTemparature(),unit:"'C"} ) );
         // Get CPU Voltages
-        conn.publish( baseTopic + "/volt/core",
-                JSON.stringify( {value:pisys.getVoltCore(),unit:"V"} ) );
-        conn.publish( baseTopic + "/volt/sdram_c",
-                JSON.stringify( {value:pisys.getVoltSdramC(),unit:"V"} ) );
-        conn.publish( baseTopic + "/volt/sdram_i",
-                JSON.stringify( {value:pisys.getVoltSdramI(),unit:"V"} ) );
-        conn.publish( baseTopic + "/volt/sdram_p",
-                JSON.stringify( {value:pisys.getVoltSdramP(),unit:"V"} ) );
-    }, 5000 );
+        conn.publish( config.topic.base + "/" +config.topic.volt_core, 
+                        JSON.stringify( {value:pisys.getVoltCore(),unit:"V"} ) );
+        conn.publish( config.topic.base + "/" +config.topic.volt_sdram_c, 
+                        JSON.stringify( {value:pisys.getVoltSdramC(),unit:"V"} ) );
+        conn.publish( config.topic.base + "/" +config.topic.volt_sdram_i, 
+                        JSON.stringify( {value:pisys.getVoltSdramI(),unit:"V"} ) );
+        conn.publish( config.topic.base + "/" +config.topic.volt_sdram_p, 
+                        JSON.stringify( {value:pisys.getVoltSdramP(),unit:"V"} ) );
+    }, 1000*config.interval );
 });
